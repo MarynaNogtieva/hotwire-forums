c4d314714d0eeaaf32845253df268e3a91e23f51
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2022-05-19 12:05:02 -0400

    Add categories to discussions index

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
   has_many :discussions, dependent: :nullify
 
   scope :sorted, -> { order(name: :asc) }
+  
+  after_create_commit -> { broadcast_prepend_to 'categories' }
 end
--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -3,12 +3,15 @@ class Discussion < ApplicationRecord
   belongs_to :category, counter_cache: true, touch: true
   has_many :posts, dependent: :destroy
 
+  # discussion.category_name
+  delegate :name, prefix: :category , to: :category, allow_nil: true
+
   validates :name, presence: true
 
   # :discussions should be consistent with the name on the index view
-  after_create_commit -> { broadcast_prepend_to :discussions }
-  after_update_commit -> { broadcast_replace_to :discussions }
-  after_destroy_commit -> { broadcast_remove_to :discussions }
+  after_create_commit -> { broadcast_prepend_to 'categories' }
+  after_update_commit -> { broadcast_replace_to 'categories' }
+  after_destroy_commit -> { broadcast_remove_to 'categories' }
 
   accepts_nested_attributes_for :posts
 
